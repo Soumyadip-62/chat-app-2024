e1fa080bdf6997c6873349c209cd7adc5ccf15f5
soumyadip <soumyadip.pandit@webskitters.in>
2024-12-02 01:00:01 +0530

    improvements

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head title="Chat-App-2024"  />
+      <Head>
+        <title>Chat-App-2024</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
